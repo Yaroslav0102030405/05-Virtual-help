f0a90162c45c7d0e95623ce0821275012864a9ea
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-01-20 13:48:03 +0200

    add animation text hero section

---
+++ care/src/js/heroText.js
@@ -0,0 +1,16 @@
+const text = document.querySelector(".sec-text");
+
+const load = () => {
+  setTimeout(() => {
+    text.textContent = "Saviour for you";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "our contact +1(437)992-7217";
+  }, 1000);
+  setTimeout(() => {
+    text.textContent = "the fastest help";
+  }, 2000);
+};
+
+load();
+setInterval(load, 6000);
--- care/src/js/main.js
+++ care/src/js/main.js
@@ -1,1 +1,9 @@
-import "../sass/style.scss";
+import styles from "../sass/style.scss?inline";
+console.log(styles);
+
+const styleEl = document.createElement("style");
+styleEl.innerHTML = styles;
+
+document.querySelector("head").append(styleEl);
+
+import "./heroText";
