b0dd6f53d35ef7e6d339cbda73a93234dc2c7145
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-01-25 13:47:44 +0200

    text animate

--- care/src/js/modal.js
+++ care/src/js/modal.js
@@ -7,11 +7,12 @@
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  refs.backdrop.addEventListener("click", logBackdropClick);
+  // refs.backdrop.addEventListener("click", logBackdropClick);
+  // refs.backdrop.addEventListener("click", closeModalBth);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
+    // document.body.classList.toggle("modal-btn-close");
     refs.backdrop.classList.toggle("is-hidden");
   }
 })();
