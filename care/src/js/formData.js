4b8a066406f3388d693e2dca6b57d99caa7bb310
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-01 16:57:22 +0200

    reset form

--- care/src/js/formData.js
+++ care/src/js/formData.js
@@ -8,6 +8,7 @@ form.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   // отменяем перезаргузку страницы при отправке формы
   event.preventDefault();
+  event.currentTarget.reset();
 
   // собираем все знаяения всех полей которые у нас есть
   // currentTarget - цу ссилка на всю форму
