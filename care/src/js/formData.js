efd5b8834ce98ca4cfe7a0b39f93793bef6629ef
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-01 17:23:34 +0200

    0

--- care/src/js/formData.js
+++ care/src/js/formData.js
@@ -8,8 +8,8 @@ form.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   // отменяем перезаргузку страницы при отправке формы
   event.preventDefault();
-  event.currentTarget.reset();
 
+  event.currentTarget.reset();
   // собираем все знаяения всех полей которые у нас есть
   // currentTarget - цу ссилка на всю форму
   const formData = new FormData(event.currentTarget);
