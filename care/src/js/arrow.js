c2af4937c35a043d31670baf9798e1760d81cef8
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-06 11:37:09 +0200

    fix arrow

--- care/src/js/arrow.js
+++ care/src/js/arrow.js
@@ -1,7 +1,26 @@
-window.addEventListener("scroll", function () {
-  var scroll = document.querySelector(".arrow");
-  scroll.classList.toggle("active", window.scrollY > 200);
-});
+// window.addEventListener("scroll", function () {
+//   var scroll = document.querySelector(".arrow");
+//   scroll.classList.toggle("active", window.scrollY > 200);
+// });
+
+// function scrollTop() {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// }
+
+// Мій код
+const arrowEl = document.querySelector(".arrow");
+window.addEventListener("scroll", arrowScroll);
+
+function arrowScroll() {
+  if (window.scrollY > 200) {
+    arrowEl.classList.add("active");
+  } else {
+    arrowEl.classList.remove("active");
+  }
+}
 
 function scrollTop() {
   window.scrollTo({
