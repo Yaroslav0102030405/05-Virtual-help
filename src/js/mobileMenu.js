cff18b2d1945a1badb269b81b1bb5a9148d2c982
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-03-17 02:55:56 +0200

    fix

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,9 +1,9 @@
-const openModal = document.querySelector(".js-open-modal");
-const closeModal = document.querySelector(".js-close-modal");
-const backdropJs = document.querySelector(".js-menu-backdrop");
+const openModal = document.querySelector('.js-open-modal');
+const closeModal = document.querySelector('.js-close-modal');
+const backdropJs = document.querySelector('.js-menu-backdrop');
 
 // Зробили клік на ссилку через деоегування подій
-const link = document.querySelector(".header__right");
+const link = document.querySelector('.header__right');
 
 // const link1 = document.querySelector(".link1");
 // const link2 = document.querySelector(".link2");
@@ -14,11 +14,11 @@ const link = document.querySelector(".header__right");
 // const link = document.querySelector('a[href="#home"]');
 
 // на єлементі вішаємо прослушиваніе подій + функії
-openModal.addEventListener("click", onOpenModal);
-closeModal.addEventListener("click", onCloseModal);
-backdropJs.addEventListener("click", onBackdropClick);
+openModal.addEventListener('click', onOpenModal);
+closeModal.addEventListener('click', onCloseModal);
+backdropJs.addEventListener('click', onBackdropClick);
 
-link.addEventListener("click", onCloseLinkClick);
+link.addEventListener('click', onCloseLinkClick);
 
 // link1.addEventListener("click", onBackdropClick);
 // link2.addEventListener("click", onBackdropClick);
@@ -29,16 +29,16 @@ link.addEventListener("click", onCloseLinkClick);
 // link.addEventListener("click", onLinkClick);
 
 function onOpenModal() {
-  document.body.classList.add("is-hidden");
-  backdropJs.classList.toggle("is-open");
+  document.body.classList.add('is-hidden');
+  backdropJs.classList.toggle('is-open');
   // openModal.classList.toggle("is-open");
   // openModal.style = "display: none";
   // backdropJs.style = "transform: translateX(0)";
 }
 
 function onCloseModal() {
-  document.body.classList.remove("is-hidden");
-  backdropJs.classList.toggle("is-open");
+  document.body.classList.remove('is-hidden');
+  backdropJs.classList.toggle('is-open');
   // closeModal.classList.toggle("is-open");
   // openModal.style = "display: block";
   // backdropJs.style = "transform: translateX(-100%)";
@@ -57,7 +57,7 @@ function onBackdropClick(event) {
 // }
 
 function onCloseLinkClick(event) {
-  if (event.target.nodeName === "A") {
+  if (event.target.nodeName === 'A') {
     onCloseModal();
     // onOpenModal();
   }
